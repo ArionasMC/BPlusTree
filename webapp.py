89515ab0beadbfdc84e68442d2afac822c025ec6
Nikolas Spyropoulos <nick.mccrafter@gmail.com>
2024-06-13 20:41:09 +0300

    added index page

--- webapp.py
+++ webapp.py
@@ -1,60 +1,47 @@
 from flask import Flask, redirect, url_for, request, render_template
 from bplustree import BPlusTree
-from tests import create_tree
 app = Flask(__name__)
 
-tree = BPlusTree(4)
+tree = None
+ttype = True # True for numeric, False for string
 
-test_tree = create_tree("trees/tree.txt")
+@app.route('/tree', methods = ['POST', 'GET'])
+def tree_page():
+    ins = ''
+    d = ''
+    if request.method == 'POST':
+        ins = request.form['insert']
+        d = request.form['delete']
+    else:
+        ins = request.args.get('insert')
+        d = request.args.get('delete')
+    
+    txt = str(ins)
+    txt_d = str(d)
+    #print(f"ins is:{ins}")
+    
+    # Process input submission
+    if tree.search(txt) == None and txt != '' and ins != None:
+        tree.insert(txt, txt)
+
+    # Process delete submission
+    if tree.search(txt_d) != None and txt_d != '' and d != None:
+        tree.delete(txt_d, txt_d)
+    return render_template('tree.html', tree_data=tree.getDictTree())
 
 @app.route('/', methods = ['POST', 'GET'])
-def test():
-    ins = ''
-    d = ''
-    if request.method == 'POST':
-        ins = request.form['insert']
-        d = request.form['delete']
-    else:
-        ins = request.args.get('insert')
-        d = request.args.get('delete')
-    
-    txt = str(ins)
-    txt_d = str(d)
-    #print(f"ins is:{ins}")
-    
-    # Process input submission
-    if tree.search(txt) == None and txt != '' and ins != None:
-        tree.insert(txt, txt)
-
-    # Process delete submission
-    if tree.search(txt_d) != None and txt_d != '' and d != None:
-        tree.delete(txt_d, txt_d)
-    return render_template('test.html', tree_data=tree.getDictTree())
-
-@app.route('/index', methods = ['POST', 'GET'])
 def index():
-    ins = ''
-    d = ''
+    global tree
+    global ttype
     if request.method == 'POST':
-        ins = request.form['insert']
-        d = request.form['delete']
-    else:
-        ins = request.args.get('insert')
-        d = request.args.get('delete')
-    
-    txt = str(ins)
-    txt_d = str(d)
-    #print(f"ins is:{ins}")
-    
-    # Process input submission
-    if tree.search(txt) == None and txt != '' and ins != None:
-        tree.insert(txt, txt)
+        tree_order = request.form['treeOrder']
+        tree_type = request.form['treeType']
+        if tree_order != None and tree_type != None:
+            tree = BPlusTree(int(tree_order))
+            ttype = str(tree_type) == 'numeric'
+            return redirect(url_for('tree_page'))
 
-    # Process delete submission
-    if tree.search(txt_d) != None and txt_d != '' and d != None:
-        tree.delete(txt_d, txt_d)
-    
-    return render_template('index.html', tree_str = str(tree).split('\n'))
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
